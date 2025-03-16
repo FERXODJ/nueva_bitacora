187e1a19f8a1f0d0ca0d668d28038c8a6a7720f6
FERXODJ <joseduvelyhuiza@gmail.com>
2025-03-16 13:59:13 -0400

    Este es para el Dominio de Vercel

--- bitacora/wsgi.py
+++ bitacora/wsgi.py
@@ -15,4 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitacora.settings')
 
 application = get_wsgi_application()
 
-app = application 
+# Exponer la variable `app` para Vercel
+app = application
